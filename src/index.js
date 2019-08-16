0ca569ccca40f369d264482d21e3ae00082be141
mkoterx <m.kote.rx@gmail.com>
2019-08-17 01:11:20 +0200

    build static version

--- src/index.js
+++ src/index.js
@@ -1,48 +1,83 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './index.scss';
 
-const products =
-    [
-        { category: "Sporting Goods", price: "$49.99", stocked: true, name: "Football" },
-        { category: "Sporting Goods", price: "$9.99", stocked: true, name: "Baseball" },
-        { category: "Sporting Goods", price: "$29.99", stocked: false, name: "Basketball" },
-        { category: "Electronics", price: "$99.99", stocked: true, name: "iPod Touch" },
-        { category: "Electronics", price: "$399.99", stocked: false, name: "iPhone 5" },
-        { category: "Electronics", price: "$199.99", stocked: true, name: "Nexus 7" }
-    ];
+const productList =
+  [
+    { category: "Sporting Goods", price: "$49.99", stocked: true, name: "Football" },
+    { category: "Sporting Goods", price: "$9.99", stocked: true, name: "Baseball" },
+    { category: "Sporting Goods", price: "$29.99", stocked: false, name: "Basketball" },
+    { category: "Electronics", price: "$99.99", stocked: true, name: "iPod Touch" },
+    { category: "Electronics", price: "$399.99", stocked: false, name: "iPhone 5" },
+    { category: "Electronics", price: "$199.99", stocked: true, name: "Nexus 7" }
+  ];
 
 function ProductRow(props) {
-    return (
-        <div>Product row</div>
-    );
+  return (
+    <div className='product-item row'>
+      <span>{props.name}</span>
+      <span>{props.price}</span>
+    </div>
+  );
 }
 
 function ProductCategoryRow(props) {
-    return (
-        <div>Product category row</div>
-    );
+  return (
+    <div className='category-heading'>{props.category}</div>
+  );
 }
 
 function SearchBar(props) {
-    return (
-        <div>Search bar</div>
-    );
+  return (
+    <div>
+      <input type='text' name='filter' /><br />
+      <input type="checkbox" name="inStock" />In Stock
+        </div>
+  );
 }
 
 function ProductTable(props) {
-    return (
-        <div>Product table</div>
-    );
+
+  const byCategory = {};
+  props.products.map(p => {
+    byCategory[p.category] = byCategory[p.category] || [];
+    byCategory[p.category].push(p);
+  });
+
+  return (
+    <div>
+      {
+        Object.keys(byCategory).map(category =>
+          (
+            <div>
+              <div className='row'>
+                <span>Name</span>
+                <span>Price</span>
+              </div>
+              <ProductCategoryRow category={category} />
+              {
+                byCategory[category].map(product =>
+                  <ProductRow name={product.name} price={product.price} />
+                )
+              }
+            </div>
+          )
+        )
+      }
+    </div>
+  );
 }
 
 function FilterableProductTable(props) {
-    return (
-        <div>Filterable product table</div>
-    );
+  return (
+    <div className='filterable-table'>
+      <SearchBar />
+      <ProductTable products={props.products} />
+    </div>
+  );
 }
 
 ReactDOM.render(
-    <div>Hello</div>,
-    document.getElementById('root')
+  <FilterableProductTable products={productList} />,
+  document.getElementById('root')
 );
